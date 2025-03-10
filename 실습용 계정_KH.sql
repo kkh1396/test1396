84b0886a0f82531ca365caaebfa74121b2f6b5c0
kkh1396 <100066522+kkh1396@users.noreply.github.com>
2025-03-10 19:58:30 +0900

    empty

--- 실습용 계정_KH.sql
+++
@@ -1,8 +0,0 @@
-select EMP_NAME as 이름 , SALARY * 12 as 연봉,(SALARY + (SALARY*BONuS))*12 as "보너스 포함 연봉" from employee;
-
-select EMP_ID, SALARY, '원' as 단위 from employee;
-
-
-
-select emp_id || emp_name from employee;
-select DISTINCT job_code from employee;
